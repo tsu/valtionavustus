f85e6627ce4eac9be09af00893c8aaf0cf780aaf
Timo Suomela <tsu@tsuzero.com>
2023-03-27 08:23:16 +0300

    Clean up after testing

--- scripts/build_and_test_jars.sh
+++ scripts/build_and_test_jars.sh
@@ -3,6 +3,7 @@ set -o errexit -o nounset -o pipefail
 source "$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )/common-functions.sh"
 
 function main {
+  trap stop_systems_under_test EXIT
   build_and_test_jars
 }
 
